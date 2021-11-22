bfc35d0def3b4ac9e026b00355957381d3201671
1234Richard <82384955+1234Richard@users.noreply.github.com>
2021-11-22 21:15:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+moustacheX = 0;
+moustacheY = 0;
+
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup() {
+    canvas = createCanvas(500, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(500, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        moustacheX = results[0].pose.nose.x - 40;
+        moustacheY = results[0].pose.nose.y;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 500, 400);
+    image(moustache, moustacheX, moustacheY, 80, 35);
+}
+
+function save() {
+    save("filterMoustacheImage");
+}
